38fd10acf11170d19be6a43c65c59d6b2c0f3e53
mustafaaziz1234 <abanana.store00@gmail.com>
2026-08-05 23:30:41 +0500

    Data types and operators

---
+++ app.py
@@ -0,0 +1,38 @@
+print("class 1")
+print(1)
+print(7+3)
+print("3+4=" , 3+4)
+name = "packman"
+print(name)
+
+#just training
+
+print("Total number of boys in class?")
+boys = (20*10)
+print("number of boys in class =" , boys)
+
+#more training 
+
+print("---Student report---")
+
+name = "mustafa"
+age = "16"
+classname = "10"
+python = "90"
+english = "80"
+maths = "70"
+
+total = int(python) + int(english) + int(maths)
+
+print("student name" , name)
+print("student age" , age)
+print("student class" , classname)
+print("student python marks" , python)
+print("student english marks" , english)
+print("student maths marks" , maths)
+print("student total marks" , total)
+
+
+
+ 
+
---
+++ app2.py
@@ -0,0 +1,22 @@
+name = "musa"
+weight = 70
+i_love_coding = True
+
+print("my name is" , name)
+print("my weight is" , weight)
+print("i love coding" , i_love_coding)
+
+print(type(name))
+print(type(weight))
+print(type (i_love_coding))
+
+#arthimatic 
+
+a = 23
+b = 5
+
+print("a+b" , a+b)
+print("a-b" , a-b)
+print("a is greater than b" , a>b)
+print("a is smaller than b" , a<b)
+print("a is equal to b", a==b)
