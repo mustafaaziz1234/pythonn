8e99df5d3d9dbe22b9297e4fca9c3076bd229026
mustafaaziz1234 <abanana.store00@gmail.com>
2026-08-06 22:48:13 +0500

    conditional statements

--- app3.py
+++ app3.py
@@ -1,5 +1,7 @@
 temp = int(input("what is the temperature?"))
-if temp>30:
-   print("the weather is mild")
+if temp <= 25:
+   print("the weather is cold")
+elif temp <= 45:
+   print("the weather is mild outside")
 else:
    print("it is too hot")
